c49c0d8591d15a68f380c73011647dc1ce548781
Valera Savchenko <ivalera.develop@gmail.com>
2023-12-10 03:01:37 +0300

    Update ui_elements.js

--- js/ui_elements.js
+++ js/ui_elements.js
@@ -1,10 +1,14 @@
 export const UI_ELEMENTS = {
     WEATHER_FROM : document.querySelector('.weather__form'),
     CITY_INPUT : document.querySelector('.weather__city'),
-    LOCATION_TEMPERATURE : document.querySelector('.weather__temperature'),
+    LOCATION_TEMPERATURE : document.querySelector('.weather__temperature-value'),
     LOCATION_NAME : document.querySelector('.weather__location'),
-    WEATHER_ICON : document.querySelector('.weather__icon'),
+    WEATHER_ICON : document.querySelector('.weather__img'),
     FAVORITE_CITY : document.querySelector('.weather__favorite-btn'),
     WEATHER_CITIES_LIST : document.querySelector('.weather__list-сities'),
-    ACTUAL_CITY : document.querySelector('.weather__location')
+    ACTUAL_CITY : document.querySelector('.weather__location'),
+    TEMPERATURE_FEELS_LIKE : document.querySelector('.weather__feels'),
+    LOCATION_SUNRISE : document.querySelector('.weather__sunrise'),
+    LOCATION_SUNSET : document.querySelector('.weather__sunset'),
+    WATHER_INTIME_LIST : document.querySelector('.weather__intime-list')
 }
