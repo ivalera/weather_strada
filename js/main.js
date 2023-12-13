5b5e9a6186ae10c29b5a8bf95de011b0d65389ef
Valera Savchenko <ivalera.develop@gmail.com>
2023-12-13 21:40:30 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,20 +1,21 @@
-import { makeRequest, getRequest } from './requests.js';
+import { makeRequest, makeRequestInTime } from './requests.js';
 import { UI_ELEMENTS } from './ui_elements.js';
 import { CONSTS_VALUES } from './consts.js';
-import { firstLetterToUpperCase, millisecondsToDate, latinToCyrillic } from './tools.js';
+import { firstLetterToUpperCase, latinToCyrillic } from './tools.js';
 import { saveCities, saveCurrentCity, getSaveCity, getSaveCitiesList} from './local_storage.js';
+import { addCitiesUI, fullFavoriteBtn } from './view.js';
+export { favoriteUpdate };
 
 let defaultCity = 'Москва';
-let favoriteCities = ['Архонская', 'Владикавказ'];
+let favoriteCities = new Set(getSaveCitiesList());
 
 UI_ELEMENTS.WEATHER_FROM.addEventListener('submit', getWeather);
-UI_ELEMENTS.FAVORITE_CITY.addEventListener('click', addFavoriteCity);
-
-favoriteCities = getSaveCitiesList(favoriteCities);
+UI_ELEMENTS.FAVORITE_CITY_BTN.addEventListener('click', addFavoriteCity);
 
 makeRequest(getSaveCity(defaultCity));
-render(favoriteCities, UI_ELEMENTS.WEATHER_CITIES_LIST);
-renderInTime(UI_ELEMENTS.WATHER_INTIME_LIST, defaultCity);
+render(Array.from(favoriteCities), UI_ELEMENTS.WEATHER_CITIES_LIST);
+renderInTime(UI_ELEMENTS.WATHER_INTIME_LIST, getSaveCity(defaultCity));
+fullFavoriteBtn(favoriteCities, getSaveCity(defaultCity), UI_ELEMENTS.FAVORITE_CITY_BTN);
 
 function getWeather(event){
     event.preventDefault();
@@ -25,6 +26,7 @@ function getWeather(event){
     
     makeRequest(cityName);
     renderInTime(UI_ELEMENTS.WATHER_INTIME_LIST, cityName);
+    fullFavoriteBtn(favoriteCities, cityName, UI_ELEMENTS.FAVORITE_CITY_BTN);
 
     event.target.reset();
 }
@@ -42,35 +44,16 @@ function removeFavoriteCities(favoriteCitiesListUI){
     }
 }
 
-function addCitiesUI(favoriteCitiesList, favoriteCitiesListUI) {
-    for (let i = 0; i < favoriteCitiesList.length; i++) {
-        const cityAdd = document.createElement("li");
-        const cityDeleteBtn = document.createElement("button");
-        const closeImgBtn = document.createElement("img");
-  
-        cityAdd.classList.add("weather__сity-item");
-        cityDeleteBtn.classList.add("weather__remove-btn");
-        cityAdd.textContent = favoriteCitiesList[i];
-        closeImgBtn.src = "./img/close-icon.svg";
-  
-        cityDeleteBtn.append(closeImgBtn);
-        cityAdd.append(cityDeleteBtn);
-        favoriteCitiesListUI.append(cityAdd);
-
-        cityAdd.addEventListener('click', favoriteUpdate);
-    }
-}
-
 function addFavoriteCity(){
     const cityActual = UI_ELEMENTS.ACTUAL_CITY.textContent;
     try {
-        const isSuchCity = favoriteCities.find(city => city === cityActual);
-        if (isSuchCity) {
+        if (favoriteCities.has(cityActual)) {
             throw new Error(CONSTS_VALUES.ERROR_SUCH_CTIY);
         }
-        favoriteCities.push(cityActual);
-        render(favoriteCities, UI_ELEMENTS.WEATHER_CITIES_LIST);
+        favoriteCities.add(cityActual);
+        render(Array.from(favoriteCities), UI_ELEMENTS.WEATHER_CITIES_LIST);
         saveCities(favoriteCities); 
+        fullFavoriteBtn(favoriteCities, cityActual, UI_ELEMENTS.FAVORITE_CITY_BTN);
     }catch(error){
         alert(error);
     }
@@ -81,19 +64,19 @@ function favoriteUpdate(event){
     const selectedCity = event.currentTarget.childNodes[0].data;
     const cityDeleteBtn = event.currentTarget.childNodes[1];
     const deleteImgBtn = event.currentTarget.childNodes[1].childNodes[0];
-  
+    
     if (event.target === clickedCity) {
         makeRequest(selectedCity);
         renderInTime(UI_ELEMENTS.WATHER_INTIME_LIST, selectedCity)
         saveCurrentCity(selectedCity);
+        fullFavoriteBtn(favoriteCities, selectedCity, UI_ELEMENTS.FAVORITE_CITY_BTN);
     }
-
     if (event.target === cityDeleteBtn || event.target === deleteImgBtn) {
         clickedCity.removeEventListener("click", favoriteUpdate);
-        favoriteCities = favoriteCities.filter(city => city !== selectedCity);
-
-        render(favoriteCities, UI_ELEMENTS.WEATHER_CITIES_LIST);
+        favoriteCities.delete(selectedCity);
+        render(Array.from(favoriteCities), UI_ELEMENTS.WEATHER_CITIES_LIST);
         saveCities(favoriteCities);
+        fullFavoriteBtn(favoriteCities, selectedCity, UI_ELEMENTS.FAVORITE_CITY_BTN);
     }
 }
 
@@ -107,49 +90,5 @@ function removeInTimeList(weatherInTimeListUI){
     while(countInTime > 0){
         weatherInTimeListUI.children[0].remove();
         countInTime--;
-    }
-}
-
-function makeRequestInTime(weatherInTimeListUI, cityName){
-    try{
-        if(!cityName) throw new(Error('No city!'));
-        getRequest(cityName, 'http://api.openweathermap.org/data/2.5/forecast')
-            .then(data =>{
-                console.log(data.list[0]);
-                сreateInTimeUI(weatherInTimeListUI, data)
-            });
-    }catch (error) {
-        alert(error);
-    }
-    
-}
-
-function сreateInTimeUI(weatherInTimeListUI, data) {
-    for (let i = 0; i <= 2; i++) {
-        const addInTime = document.createElement("li");
-        const weatherCurrent = document.createElement("ul");
-        const weatherTitle = document.createElement("li");
-        const weatherTemperature = document.createElement("li");
-        const weatherFeels = document.createElement("li");
-        const weatherImg = document.createElement("img");
-  
-        addInTime.classList.add("weather__intime-elem");
-        weatherCurrent.classList.add("weather__intime-current");
-        weatherTitle.classList.add("weather__intime-title");
-        weatherTemperature.classList.add("weather__intime-temperature");
-        weatherFeels.classList.add("weather__intime-feels");
-        weatherImg.classList.add("weather__img-current");
-        
-        weatherTitle.textContent = millisecondsToDate(data.list[i].dt);
-        weatherTemperature.textContent = "Температура: " + data.list[i].main.temp.toFixed() + CONSTS_VALUES.DEGREE_SYMBOL;
-        weatherFeels.textContent = "Ощущается как: " + data.list[i].main.feels_like.toFixed() + CONSTS_VALUES.DEGREE_SYMBOL;
-        weatherImg.src = `https://openweathermap.org/img/wn/${data.list[i].weather[0].icon}@2x.png`;
-  
-        addInTime.append(weatherCurrent);
-        weatherCurrent.append(weatherTitle);
-        weatherCurrent.append(weatherTemperature);
-        weatherCurrent.append(weatherFeels);
-        addInTime.append(weatherImg);
-        weatherInTimeListUI.append(addInTime);
     }
 }
