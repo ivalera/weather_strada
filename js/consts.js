52d76dd8acf704408462ec9438fdec1cdc1a54c1
Valera Savchenko <ivalera.develop@gmail.com>
2023-12-16 13:57:18 +0300

    Update consts.js

--- js/consts.js
+++ js/consts.js
@@ -1,5 +1,6 @@
 export const CONSTS_VALUES = {
     ERROR_SUCH_CTIY : "Добавляемый город, уже есть в избарнных.",
     ERROR_EMPTY_INPUT : "Поле для ввода не должно быть пустым. Введите город.",
-    DEGREE_SYMBOL : "°"
+    DEGREE_SYMBOL : "°",
+    EMPTY_VALUE : ""
 }
