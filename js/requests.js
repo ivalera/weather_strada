4cf5302b18b2a71f50d5092c111bbb6ca9faafb5
Valera Savchenko <ivalera.develop@gmail.com>
2023-12-16 13:58:10 +0300

    Update requests.js

--- js/requests.js
+++ js/requests.js
@@ -8,10 +8,10 @@ const serverUrl = 'http://api.openweathermap.org/data/2.5/weather';
 const apiKey = '80b3669790743bca0100b598dba76aa2';
 const forecastUrl = 'http://api.openweathermap.org/data/2.5/forecast';
 
-function getRequest(city, url) {
-	return fetch(`${url}?q=${city}&appid=${apiKey}&units=metric`)
-		.then(response => { 
-			switch(response.status){
+async function getRequest(city, url) {
+	const response = await fetch(`${url}?q=${city}&appid=${apiKey}&units=metric`);
+	try{
+		switch (response.status) {
 			case 400:
 		        throw new Error('Bad Request - неправильный запрос от клиента');
 			case 404:
@@ -19,39 +19,35 @@ function getRequest(city, url) {
 			case 500:
 		        throw new Error('Internal Server Error - ошибка сервера ');
 		    default: 
-				return response.json();
-			}
-	    })
-		.catch((error) => alert(error));
-}
-
-function makeRequest(cityName){
-	try {
-		if (!cityName) throw new Error(CONSTS_VALUES.ERROR_EMPTY_INPUT);
-		getRequest(cityName, serverUrl)
-			.then(data => {
-				console.log(data);
-				UI_ELEMENTS.LOCATION_TEMPERATURE.textContent = removeMinuse(data.main.temp.toFixed());
-				UI_ELEMENTS.LOCATION_NAME.textContent = cityName;
-				UI_ELEMENTS.WEATHER_ICON.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
-				UI_ELEMENTS.TEMPERATURE_FEELS_LIKE.textContent = removeMinuse(data.main.feels_like.toFixed()) + CONSTS_VALUES.DEGREE_SYMBOL;
-				UI_ELEMENTS.LOCATION_SUNRISE.textContent = millisecondsToDate(data.sys.sunrise);
-				UI_ELEMENTS.LOCATION_SUNSET.textContent = millisecondsToDate(data.sys.sunset);
-			});
-			UI_ELEMENTS.CITY_INPUT.value = '';
-	} catch (error) {
+		 		return await response.json();
+		}
+	}catch (error) { 
 		alert(error);
 	}
 }
 
-function makeRequestInTime(weatherInTimeListUI, cityName){
+async function makeRequest(cityName){
+	try {
+		if (!cityName) throw new Error(CONSTS_VALUES.ERROR_EMPTY_INPUT);
+		const data = await getRequest(cityName, serverUrl);
+		UI_ELEMENTS.LOCATION_TEMPERATURE.textContent = removeMinuse(data.main.temp.toFixed());
+		UI_ELEMENTS.LOCATION_NAME.textContent = cityName;
+		UI_ELEMENTS.WEATHER_ICON.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+		UI_ELEMENTS.TEMPERATURE_FEELS_LIKE.textContent = removeMinuse(data.main.feels_like.toFixed()) + CONSTS_VALUES.DEGREE_SYMBOL;
+		UI_ELEMENTS.LOCATION_SUNRISE.textContent = millisecondsToDate(data.sys.sunrise);
+		UI_ELEMENTS.LOCATION_SUNSET.textContent = millisecondsToDate(data.sys.sunset);
+		UI_ELEMENTS.CITY_INPUT.value = CONSTS_VALUES.EMPTY_VALUE;
+	}catch (error) {
+		alert(error);
+	}
+}
+
+async function makeRequestInTime(weatherInTimeListUI, cityName){
     try{
-        if(!cityName) throw new Error(CONSTS_VALUES.ERROR_EMPTY_INPUT);
-        getRequest(cityName, forecastUrl)
-            .then(data =>{
-                console.log(data.list[0]);
-                сreateInTimeUI(weatherInTimeListUI, data)
-            });
+        if (!cityName) throw new Error(CONSTS_VALUES.ERROR_EMPTY_INPUT);
+
+        const data = await getRequest(cityName, forecastUrl);
+        сreateInTimeUI(weatherInTimeListUI, data);
     }catch (error) {
         alert(error);
     } 
